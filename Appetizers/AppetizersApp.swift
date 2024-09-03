ded6f99d1b9b27432f794aabf48a3b9317aac5d2
Himy Mughal <himyiosdev@gmail.com>
2024-09-03 15:21:03 +0500

    first push

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Hamza Mughal on 03/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
